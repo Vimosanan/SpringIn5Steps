2518ef3d6f19ad2820279bd5be6ea35945ecca13
Vimosanan <vimosanan.13@cse.mrt.ac.lk>
2018-03-27 22:59:38 +0530

    First commit

---
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
@@ -0,0 +1,23 @@
+package com.in28minutes.spring.basics.springin5steps;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BinarySearchImpl {
+
+	@Autowired
+	private SortAlgorithm sortAlgorithm;
+
+	public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
+		super();
+		this.sortAlgorithm = sortAlgorithm;
+	}
+
+	public int binarySearch(int[] numbers, int searchedNumber) {
+		// BubbleSortAlgorithm bubbleSortAlgo = new BubbleSortAlgorithm();
+		int[] sortedNumbers = sortAlgorithm.sort(numbers);
+		// Searching algorithm
+		return 3;
+	}
+}
---
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/QuickSortAlgorithm.java
@@ -0,0 +1,9 @@
+package com.in28minutes.spring.basics.springin5steps;
+
+public class QuickSortAlgorithm implements SortAlgorithm {
+	@Override
+	public int[] sort(int[] numbers) {
+		// Logic of Quick sort algorithm
+		return numbers;
+	}
+}
