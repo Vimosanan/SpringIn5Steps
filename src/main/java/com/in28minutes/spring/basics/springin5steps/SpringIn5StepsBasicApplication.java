999e28dcf1308f6202bb659a754fa3740064ab28
Vimosanan <vimosanan.13@cse.mrt.ac.lk>
2018-03-28 21:28:13 +0530

    Spring singlton bean added

--- src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
@@ -4,14 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import com.in28minutes.spring.basics.springin5steps.basic.BinarySearchImpl;
+
 @SpringBootApplication
-public class SpringIn5StepsApplication {
+public class SpringIn5StepsBasicApplication {
 
 	public static void main(String[] args) {
 		// BinarySearchImpl binarySearch = new BinarySearchImpl(new
 		// QuickSortAlgorithm());
 
-		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsBasicApplication.class, args);
 		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
 		int result = binarySearch.binarySearch(new int[] { 12, 4, 3, 5 }, 3);
 		System.out.println("Result is " + result);
